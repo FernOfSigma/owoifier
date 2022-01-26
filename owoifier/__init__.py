d5f925f3502ef1dfb98bd5cc5f16de834c50fcc8
FernOfSigma <90320595+FernOfSigma@users.noreply.github.com>
2022-01-27 01:53:03 +0530

    Fix typo

--- owoifier/__init__.py
+++ owoifier/__init__.py
@@ -12,7 +12,7 @@
 # A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License along with
-# owoifer. If not, see <https://www.gnu.org/licenses/>.
+# owoifier. If not, see <https://www.gnu.org/licenses/>.
 
 from .owoifier import owoify
 
